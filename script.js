ba9d24a8afce16ca281dde1acf9d9ce0364291ed
ricky <oparaiskelechi@gmail.com>
2018-08-13 00:43:24 +0100

    fix the script.js file

--- script.js
+++ script.js
@@ -14,15 +14,13 @@ const disable =()=> {
 	} else {
 		submitButton.setAttribute('disable', 'true');
 		submitButton.style.background = "#f2f2f2";
+    //document.getElementById("sample").innerHTML = "";
 	}
 }
 
-	/*function alert() {
-	console.log("please enter a 3 digit number");
-	*/
-const errorAlert = ()=> {
+/*const errorAlert = ()=> {
 	alert("Please enter a 3 digit number");
-}
+}*/
 
 
 const armstrong = ()=> {
@@ -31,41 +29,53 @@ const armstrong = ()=> {
 	let numberLength = splitInputNumber.length;
 	let submitButton = document.getElementById("btn");
 	let sum;
-
+  //checking if the submit button is enable
 	if(submitButton.enabled = true) {
+    //checking if input field is empty.
 		if(inputNumber === "" || inputNumber === " ") {
-			document.getElementById("error").innerHTML = "Please enter your values";
-			document.getElementById("error").style.color = "red";
-		} else if(isNaN(inputNumber)) {
+		  document.getElementById("error").innerHTML = "Please enter your values";
+		  document.getElementById("error").style.color = "red";
+	      document.getElementById("sample").innerHTML = "";
+	      document.getElementById("result").innerHTML = "";
+		} // checking input number is not a number
+    else if(isNaN(inputNumber)) {
 			document.getElementById("error").innerHTML = "Numbers Only Please";
 			document.getElementById("error").style.color = "red";
-		} else if(numberLength > 3 || numberLength < 3) {
+      		document.getElementById("sample").innerHTML = "";
+      		document.getElementById("result").innerHTML = "";
+		}//checking if the inputed number is less or greater than 3. 
+    else if(numberLength > 3 || numberLength < 3) {
+      		document.getElementById("sample").innerHTML = "";
 			document.getElementById("error").innerHTML = "Please number should be 3 digits only";
 			document.getElementById("error").style.color = "red";
+      		document.getElementById("result").innerHTML = "";
 			submitButton.enabled = false;
 		} else {
 			/*for(i=0; i == numberLength; i++){
                 sum = splitInputNumber[i]**3 + sum;
              }*/
+  //initializing the new array to the power of 3.
+      let num1 = splitInputNumber[0]**3;
+      let num2 = splitInputNumber[1]**3;
+      let num3 = splitInputNumber[2]**3;
 
-
-           	let num1 = splitInputNumber[0]**3;
-			let num2 = splitInputNumber[1]**3;
-			let num3 = splitInputNumber[2]**3;
-
-			 sum = num1 + num2 + num3;
-
-             //document.getElementById("count").innerHTML = splitInputNumber;
-             if(sum == inputNumber) {
-             	document.getElementById("error").innerHTML = "";
-             	document.getElementById("result").innerHTML = inputNumber + " is an Armstrong number";
-             	document.getElementById("digits").value = "";
-             } else {
-             	document.getElementById("error").innerHTML = "Wrong Number!!"
-             	document.getElementById("result").innerHTML = inputNumber + " is not an Armstrong number";
-             }
+			 sum = num1 + num2 + num3; 
+            
+  //checking if the input number is an Armstrong number
+      if(sum == inputNumber) {
+        document.getElementById("error").innerHTML = "";
+        document.getElementById("result").innerHTML = `${inputNumber} is an Armstrong number`;
+        document.getElementById("sample").innerHTML = `${splitInputNumber[0]}**3 + ${splitInputNumber[1]}**3 + ${splitInputNumber[2]}**3 = ${inputNumber}`;
+        document.getElementById("digits").value = "";
+      } else {
+        document.getElementById("sample").innerHTML = "";
+        document.getElementById("error").innerHTML = "Wrong Number!!";
+        document.getElementById("error").style.color = "red";
+        document.getElementById("result").innerHTML = `${inputNumber} is not an Armstrong number`;
+        document.getElementById("digits").value = "";
+      }
 		}
 	} else {
-		errorAlert();
+		alert("Please enter a 3 digit number");
 	}
 }
